6daad455fc7d1e912be2791526b40c978e76e4b6
Vinicius Lira <vinicius.lira@riachuelo.com.br>
2023-02-23 23:19:33 -0300

    start screen login

---
+++ react_front/src/Components/Login/index.js
@@ -0,0 +1,22 @@
+import './styles.css';
+export default function Login() {
+    return (
+        <>
+            <div>
+                <img
+                    className='subCotainer'
+                    src="https://avatars.githubusercontent.com/u/62779278?s=96&v=4"
+                    alt="Girl in a jacket"
+                    width="100"
+                    height="100"
+                />
+                <div className="">
+                    <p>Usuário</p>
+                    <input />
+                    <p>Senha</p>
+                    <input />
+                </div>
+            </div>
+        </>
+    );
+}
